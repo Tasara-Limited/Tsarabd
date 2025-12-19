12fa3fd9a31a9809f53dcf61f3c55b0e3cbca4f5
Tasara-Limited <tasarait@gmail.com>
2025-12-19 20:03:12 +0600

    Add files via upload

---
+++ sitemap.xml/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+  <url>
+    <loc>https://www.tasarabd.com/</loc>
+    <priority>1.0</priority>
+  </url>
+
+  <url>
+    <loc>https://www.tasarabd.com/about</loc>
+    <priority>0.8</priority>
+  </url>
+
+  <url>
+    <loc>https://www.tasarabd.com/services</loc>
+    <priority>0.8</priority>
+  </url>
+
+  <url>
+    <loc>https://www.tasarabd.com/contact</loc>
+    <priority>0.5</priority>
+  </url>
+
+</urlset>`;
+
+  return new NextResponse(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
